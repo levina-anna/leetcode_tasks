f04c954168393dab39864aee8fac733cd94ae126
anna-levina <anna_levina@internet.ru>
2024-02-16 08:40:00 +0300

    Добавлен two_sum.py

---
+++ leetcode/two_sum.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        prevMap = {}
+
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in prevMap:
+                return [prevMap[diff], i]
+            prevMap[n] = i
+
+
+nums = [2, 7, 11, 15]
+target = 9
+
+result = Solution().twoSum(nums, target)
+print(result)
