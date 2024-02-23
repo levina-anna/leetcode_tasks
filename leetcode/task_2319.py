14c6edac3ce0df123600dcd51df907f5418db4ab
anna-levina <anna_levina@internet.ru>
2024-02-23 19:22:15 +0300

    Добавлен task_2319.py

---
+++ leetcode/task_2319.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def checkXMatrix(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: bool
+        """
+        n = len(grid)
+        for i in range(n):
+            for j in range(n):
+                if i == j or i + j == n - 1:  # На диагоналях
+                    if grid[i][j] == 0:
+                        return False
+                else:  # Вне диагоналей
+                    if grid[i][j] != 0:
+                        return False
+        return True
+
+# Пример тестирования решения
+sol = Solution()
+grid = [
+    [2, 0, 0, 1],
+    [0, 3, 1, 0],
+    [0, 5, 2, 0],
+    [4, 0, 0, 2]
+]
+
+sol.checkXMatrix(grid)
