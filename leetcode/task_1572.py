2e2b35f4d893d517a34a1b67788fd9770764d309
anna-levina <anna_levina@internet.ru>
2024-02-24 22:34:26 +0300

    Обнволен task_1572.py

--- leetcode/task_1572.py
+++ leetcode/task_1572.py
@@ -1,4 +1,4 @@
-class Solution(object):
+class Solution:
     def diagonalSum(self, mat):
         """
         :type mat: List[List[int]]
@@ -7,10 +7,20 @@ class Solution(object):
         n = len(mat)
         total_sum = 0
         for i in range(n):
-            total_sum += mat[i][i]
-            total_sum += mat[i][n - 1 - i]
+            total_sum += mat[i][i]  # Главная диагональ
+            total_sum += mat[i][n - 1 - i]  # Побочная диагональ
 
-        if n % 2 == 1:
+        if n % 2 == 1:  # Если размер матрицы нечетный, вычесть центральный элемент, так как он был добавлен дважды
             total_sum -= mat[n // 2][n // 2]
 
         return total_sum
+
+
+# Test the solution
+sol = Solution()
+mat = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+print(sol.diagonalSum(mat))
