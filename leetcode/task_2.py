b0a2f4faca6bb432087dea07389181f37b91caec
anna-levina <anna_levina@internet.ru>
2024-03-01 08:34:18 +0300

    Добавлен task_2.py

---
+++ leetcode/task_2.py
@@ -0,0 +1,33 @@
+# Определение класса для узла связанного списка
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        carry = 0
+        root = ListNode(0)
+        current = root
+
+        while l1 or l2 or carry:
+            val1 = (l1.val if l1 else 0)
+            val2 = (l2.val if l2 else 0)
+            total = val1 + val2 + carry
+
+            carry = total // 10
+            current.next = ListNode(total % 10)
+
+            current = current.next
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
+
+        return root.next
