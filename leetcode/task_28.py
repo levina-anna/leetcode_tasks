cc7703a619d09565360e5016ad7f2ca10fba077b
anna-levina <anna_levina@internet.ru>
2024-03-13 19:58:31 +0300

    Добавлен task_28.py

--- leetcode/task_28.py
+++ leetcode/task_28.py
@@ -5,11 +5,15 @@ class Solution(object):
         :type needle: str
         :rtype: int
         """
+        # Если needle — пустая строка, возвращаем 0, как указано в условии задачи
         if not needle:
             return 0
 
+        # Проверяем каждую подстроку haystack, которая имеет такую же длину, как needle
         for i in range(len(haystack) - len(needle) + 1):
+            # Если подстрока совпадает с needle, возвращаем её начальный индекс
             if haystack[i:i + len(needle)] == needle:
                 return i
 
+        # Если needle не найден в haystack, возвращаем -1
         return -1
