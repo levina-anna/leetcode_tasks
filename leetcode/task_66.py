08367395b7eb1262e2f839d2454d6d97d7ee562a
anna-levina <anna_levina@internet.ru>
2024-03-24 22:38:57 +0300

    Добавлен task_66.py

---
+++ leetcode/task_66.py
@@ -0,0 +1,24 @@
+class Solution:
+    def plusOne(self, digits):
+        digits = digits[::-1]
+
+        carry, i = 1, 0
+        while carry:
+            if i < len(digits):
+                if digits[i] < 9:
+                    digits[i] += carry
+                    carry = 0
+                else:
+                    digits[i] = 0
+            else:
+                digits.append(carry)
+                carry = 0
+            i += 1
+
+        return digits[::-1]
+
+
+# Example usage
+solution = Solution()
+result = solution.plusOne([9, 9, 9])
+print(result)
