ce0afdb8fd8d98d97ffb0ba387e8e970a439f00d
anna-levina <anna_levina@internet.ru>
2024-04-03 21:19:54 +0300

    Добавлен task_69.py

---
+++ leetcode/task_69.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        x = int(x ** 0.5)
+        return x
+
+
+solution = Solution()
+result = solution.mySqrt(4)
+
+print(result)
+print(int(3.7))
+
+
