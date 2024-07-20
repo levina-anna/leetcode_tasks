b8e657498bf1fce84cdd247cc7d9400e4d811b21
Anna Levina <anna_levina@internet.ru>
2024-07-20 22:51:32 +0300

    task_1605.py

---
+++ python_tasks/task_1605.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def restoreMatrix(self, rowSum, colSum):
+        """
+        :type rowSum: List[int]
+        :type colSum: List[int]
+        :rtype: List[List[int]]
+        """
+        matrix = [[0] * len(colSum) for _ in range(len(rowSum))]
+
+        for i in range(len(rowSum)):
+            for j in range(len(colSum)):
+                matrix[i][j] = min(rowSum[i], colSum[j])
+
+                rowSum[i] -= matrix[i][j]
+                colSum[j] -= matrix[i][j]
+
+        return matrix
