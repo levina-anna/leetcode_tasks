2c2b5e77a9cc47c2bb30ae6a32dabc05ceaded25
Anna Levina <anna_levina@internet.ru>
2024-07-29 20:25:06 +0300

    task_1395.py

--- python_tasks/task_1395.py
+++ python_tasks/task_1395.py
@@ -4,17 +4,27 @@ class Solution(object):
         :type rating: List[int]
         :rtype: int
         """
-        count = 0
         n = len(rating)
+        count = 0
 
-        for i in range(n):
-            for j in range(i + 1, n):
-                for k in range(j + 1, n):
+        for j in range(1, n - 1):
+            left_smaller = 0
+            left_larger = 0
+            right_smaller = 0
+            right_larger = 0
 
-                    if rating[i] < rating[j] < rating[k]:
-                        count += 1
+            for i in range(j):
+                if rating[i] < rating[j]:
+                    left_smaller += 1
+                elif rating[i] > rating[j]:
+                    left_larger += 1
 
-                    if rating[i] > rating[j] > rating[k]:
-                        count += 1
+            for k in range(j + 1, n):
+                if rating[k] < rating[j]:
+                    right_smaller += 1
+                elif rating[k] > rating[j]:
+                    right_larger += 1
+
+            count += left_smaller * right_larger + left_larger * right_smaller
 
         return count
