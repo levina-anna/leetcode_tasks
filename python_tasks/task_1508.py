71daacc36d89a18cb81c14ed9cf7d3152c4a86e9
Anna Levina <anna_levina@internet.ru>
2024-08-04 19:00:22 +0300

    task_1508.py

---
+++ python_tasks/task_1508.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def rangeSum(self, nums, n, left, right):
+        """
+        :type nums: List[int]
+        :type n: int
+        :type left: int
+        :type right: int
+        :rtype: int
+        """
+        import heapq
+
+        subarray_sums = []
+        for i in range(n):
+            current_sum = 0
+            for j in range(i, n):
+                current_sum += nums[j]
+                subarray_sums.append(current_sum)
+
+        subarray_sums.sort()
+        result = sum(subarray_sums[left - 1:right])
+
+        return result
